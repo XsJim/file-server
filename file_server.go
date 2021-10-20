596319900180680a73b4bae4424132ec41747678
XsJim <shixinsheng@live.cn>
2021-10-20 20:52:50 +0800

    添加了存放无类型文件的文件夹的预定义名，添加文件服务器文件名数据库的名称，表示 redis 中对应集合的名字，注释并添加新的根路径是为了在开发机测试

--- file_server.go
+++ file_server.go
@@ -15,8 +15,12 @@ var (
 
 	redisAddr     = flag.String("redisAddr", "59.110.218.108:10324", "redis服务地址")
 	redisPassword = flag.String("redisPassword", "S5sdf65FxvhSDFo5", "redis密码")
+	// 在程序进行服务前，应该创建这个路径
+	//fileRootDir = flag.String("fileRootDir", "/home/xsjim/file_server_dir", "本地文件主目录")
+	fileRootDir   = flag.String("fileRootDir", "X:/file", "本地文件主目录")
+	emptyTypeFile = flag.String("emptyTypeFile", "emptyType", "用来放置没有类型的文件的文件夹，这个文件夹会被自动创建")
 
-	fileRootDir = flag.String("fileRootDir", "/home/xsjim/file_server_dir/", "本地文件主目录")
+	setName = flag.String("setName", "fileNameSet", "用于存放文件名的集合的名字")
 )
 
 var (
