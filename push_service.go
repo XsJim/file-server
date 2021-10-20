45a1ee0c0d1bb235b41612e316610ee382bfcc81
XsJim <shixinsheng@live.cn>
2021-10-20 20:50:24 +0800

    push_service 修改一些错误，进行了测试，多数情况都能够正确接收文件并保存，在测试的五六次中，出现一回 pdf 文件传输之后无法打开的情况，和原文件大小相同，暂时未发现出错原因

--- push_service.go
+++ push_service.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"log"
 	"net"
 	"os"
@@ -28,41 +29,52 @@ func pushService(conn net.Conn) {
 	bytes = make([]byte, bytes[0])
 
 	// 读入文件类型标识
-	conn.Read(bytes)
-
-	// 将文件名转换成字符串形式
+	_, err = conn.Read(bytes)
+	checkErrorPrint(err)
+	// 将文件类型标识转换成字符串形式
 	fileType := string(bytes)
 
+	// 读取文件 byte 尺寸，长度协商为 4 位
+	bytes = make([]byte, 4)
+	_, err = conn.Read(bytes)
+	checkErrorPrint(err)
+	byteSize := ByteSizeFromByteSlice(bytes)
+
 	// 将图片字节数组读入 bytes 中
-	bytes = make([]byte, 0)
-	temp := make([]byte, 4096)
-
-	for n, err := conn.Read(temp); n != 0; {
-		checkErrorPrint(err)
-		bytes = append(bytes, temp[:n]...)
-		n, err = conn.Read(temp)
-	}
-
+	bytes = make([]byte, byteSize)
+	_, err = conn.Read(bytes)
+	checkErrorPrint(err)
 	// 生成一个文件名（uuid + . + fileType）
 	fileName := uuid.New().String() + "." + fileType
 
 	// 将文件名保存到 redis ，便于后续确定文件是否存在
 	redisConn := redisPoll.Get()
 	defer redisConn.Close()
-	if changeRow, err := redis.Int(redisConn.Do("SADD", fileName)); changeRow == 0 || err != nil {
+	if changeRow, err := redis.Int(redisConn.Do("SADD", *setName, fileName)); changeRow == 0 || err != nil {
 		log.Println(err)
 		return
 	}
 
 	// 将文件保存到文件夹
 	go writeFile(fileName, fileType, bytes)
-	// 将文件名写回给客户
-	conn.Write([]byte(fileName))
+	// 将文件名写回给客户，加上文件类型
+	_, err = conn.Write([]byte(fileName))
+	checkErrorPrint(err)
 }
 
 func writeFile(fileName string, fileType string, bytes []byte) {
+	if fileType == "" {
+		fileType = *emptyTypeFile
+	}
+	// 首先检查是否有该类型的文件夹，如果没有，创建一个
+	if !DirExist(fileType) {
+		MkDir(fileType)
+	}
 	outPutFile, outPutError := os.OpenFile(*fileRootDir+"/"+fileType+"/"+fileName, os.O_WRONLY|os.O_CREATE, 0666)
-	checkErrorPrint(outPutError)
+	if outPutError != nil {
+		log.Println(outPutError)
+		return
+	}
 
 	defer outPutFile.Close()
 
@@ -70,5 +82,27 @@ func writeFile(fileName string, fileType string, bytes []byte) {
 
 	_, err := outPutWrite.Write(bytes)
 	checkErrorPrint(err)
-	outPutWrite.Flush()
+	err = outPutWrite.Flush()
+	checkErrorPrint(err)
+}
+
+// DirExist 检查文件夹是否存在
+func DirExist(dirName string) (exist bool) {
+	_, err := os.Stat(*fileRootDir + "/" + dirName)
+	return err == nil || errors.Is(err, os.ErrExist)
+}
+
+// MkDir 创建一个不存在的目录
+func MkDir(dirName string) {
+	err := os.Mkdir(*fileRootDir+"/"+dirName, 0666)
+	checkErrorPrint(err)
+}
+
+// ByteSizeFromByteSlice 从一个 byte 切片中组合出一个表示字节数量的 int
+func ByteSizeFromByteSlice(tar []byte) (byteSize int) {
+	for i := 0; i < 4; i++ {
+		byteSize |= int(tar[i]) << (8 * i)
+	}
+
+	return
 }
